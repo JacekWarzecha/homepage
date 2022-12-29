564636bdbdfbe95fb407bc166f6344d04744aa73
JacekWarzecha <drjackys@gmail.com>
2022-12-29 22:23:30 +0100

    icon, OG and JS added

--- js/script.js
+++ js/script.js
@@ -1,71 +1,119 @@
-console.log("hi");
+{
+  {
+    const welcome = () => {
+      console.log("hi");
+    };
 
-let buttonLove = document.querySelector(".js-button-love");
-let articleOfMoney = document.querySelector(".js-sectionLoveMoney-money");
-
-let buttonMoney = document.querySelector(".js-button-money");
-let articleOfLove = document.querySelector(".js-sectionLoveMoney-love");
-
-let buttonTheme = document.querySelector(".js-header__button");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".js-themeName");
-let navigationUnList = document.querySelector(".js-navigation-list");
-let containerList = document.querySelector(".js-container-list");
-
-let buttonImg = document.querySelector(".js-sectionLoveMoney-button");
-let IMG = document.querySelector(".js-sectionLoveMoney__photo");
-
-let sectionLoveMoney__photo = document.querySelector(
-  ".js-sectionLoveMoney-photo"
-);
-
-let containerCrypto__Variable = document.querySelector(
-  ".js-containerCrypto__Variable"
-).innerText;
-
-let containerCrypto__howMuch = document.querySelector(
-  ".js-containerCrypto__howMuch"
-);
-
-buttonLove.addEventListener("click", () => {
-  articleOfMoney.remove();
-});
-
-buttonMoney.addEventListener("click", () => {
-  articleOfLove.remove();
-});
-
-buttonTheme.addEventListener("click", () => {
-  body.classList.toggle("js-header__dark");
-  navigationUnList.classList.toggle("js-navigation__outline");
-  containerList.classList.toggle("js-containerList__backgroundcolor");
-
-  themeName.innerText = body.classList.contains("js-header__dark")
-    ? "jasny"
-    : "ciemny";
-});
-
-buttonImg.addEventListener("click", () => {
-  IMG.remove();
-  if (
-    sectionLoveMoney__photo.classList.contains("js-sectionLoveMoney-button")
-  ) {
-    buttonImg.classList.add("js-sectionLoveMoney-text-decoration");
-  } else {
-    buttonImg.remove();
+    welcome();
   }
-});
+  //--------------------------------------------------------
+  //-----------------USUWANIE MONEY & LOVE
+  {
+    const articleOfMoney = document.querySelector(".js-sectionLoveMoney-money");
+    const articleOfLove = document.querySelector(".js-sectionLoveMoney-love");
 
-switch (containerCrypto__Variable) {
-  case "-25":
-    containerCrypto__howMuch.innerText = "+275";
-    break;
-  case "-50":
-    containerCrypto__howMuch.innerText = "+250";
-    break;
-  case "+100":
-    containerCrypto__howMuch.innerText = "400";
-    break;
-  default:
-    containerCrypto__howMuch.innerText = "300";
+    const deleteMoney = () => {
+      articleOfMoney.remove();
+    };
+
+    const initDeleteMoney = () => {
+      const buttonLove = document.querySelector(".js-button-love");
+      buttonLove.addEventListener("click", deleteMoney);
+    };
+
+    const deleteLove = () => {
+      articleOfLove.remove();
+    };
+
+    const initDeleteLove = () => {
+      const buttonMoney = document.querySelector(".js-button-money");
+      buttonMoney.addEventListener("click", deleteLove);
+    };
+
+    initDeleteMoney();
+    initDeleteLove();
+
+    // Chciałem zrobić coś takiego (żeby było mniej kodu) ale nie działało
+    // const deleteLove = () => {
+    //   articleOfLove.remove();
+    //   buttonMoney.addEventListener("click", deleteLove);
+    // };
+  }
+  //-------------------------------------------------
+  //-------------------- ZMIANA MOTYWU
+  {
+    const changeTheme = () => {
+      const navigationUnList = document.querySelector(".js-navigation-list");
+      const themeName = document.querySelector(".js-themeName");
+      const body = document.querySelector(".body");
+      const containerList = document.querySelector(".js-container-list");
+
+      body.classList.toggle("js-header__dark");
+      navigationUnList.classList.toggle("js-navigation__outline");
+      containerList.classList.toggle("js-containerList__backgroundcolor");
+
+      themeName.innerText = body.classList.contains("js-header__dark")
+        ? "jasny"
+        : "ciemny";
+    };
+
+    const initChangeTheme = () => {
+      const buttonTheme = document.querySelector(".js-header__button");
+      buttonTheme.addEventListener("click", changeTheme);
+    };
+
+    initChangeTheme();
+  }
+  //------------------------------------------------
+  //-----------------USUWANIE ZDJĘCIA
+  {
+    const buttonImg = document.querySelector(".js-sectionLoveMoney-button");
+    const photoDelete = () => {
+      const IMG = document.querySelector(".js-sectionLoveMoney__photo");
+      const sectionLoveMoneyPhoto = document.querySelector(
+        ".js-sectionLoveMoney-photo"
+      );
+      IMG.remove();
+      if (
+        sectionLoveMoneyPhoto.classList.contains("js-sectionLoveMoney-button")
+      ) {
+        buttonImg.classList.add("js-sectionLoveMoney-text-decoration");
+      } else {
+        buttonImg.remove();
+      }
+    };
+
+    const initPhotoDelete = () => {
+      buttonImg.addEventListener("click", photoDelete);
+    };
+
+    initPhotoDelete();
+  }
+  //------------------------------------------------------------------------
+  //-------------------ZMIANA ZYSKÓW
+  {
+    const switchIncome = () => {
+      const containerCryptoVariable = document.querySelector(
+        ".js-containerCrypto-variable"
+      ).innerText;
+
+      const containerCrypto__howMuch = document.querySelector(
+        ".js-containerCrypto-howMuch"
+      );
+      switch (containerCryptoVariable) {
+        case "-25":
+          containerCrypto__howMuch.innerText = "+275";
+          break;
+        case "-50":
+          containerCrypto__howMuch.innerText = "+250";
+          break;
+        case "+100":
+          containerCrypto__howMuch.innerText = "400";
+          break;
+        default:
+          containerCrypto__howMuch.innerText = "300";
+      }
+    };
+    switchIncome();
+  }
 }
